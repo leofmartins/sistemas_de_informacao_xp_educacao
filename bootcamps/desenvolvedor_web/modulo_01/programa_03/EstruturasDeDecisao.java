b3e564f2fd927d63ee3a9f43f8121cd22b61973c
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-10-17 17:43:32 -0300

    início programa 03

---
+++ bootcamps/desenvolvedor_web/modulo_01/programa_03/EstruturasDeDecisao.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EstruturasDeDecisao {
+
+    public static void main(String[] args) {
+        int aux;
+        boolean avista, carroNovo, seguir;
+        double desconto, valorCarro;
+
+        Scanner Entrada = new Scanner(Entrada.nextInt())
+    }
+}
