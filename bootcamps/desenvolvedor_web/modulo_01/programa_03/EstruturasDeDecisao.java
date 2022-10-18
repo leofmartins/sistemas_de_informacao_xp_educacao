a9ae3d10ef4f9810f26180a4839236def7c7beec
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-10-18 13:00:08 -0300

    corrigido programa 03

--- bootcamps/desenvolvedor_web/modulo_01/programa_03/EstruturasDeDecisao.java
+++ bootcamps/desenvolvedor_web/modulo_01/programa_03/EstruturasDeDecisao.java
@@ -7,43 +7,49 @@ public class EstruturasDeDecisao {
         boolean avista, carroNovo, seguir;
         double desconto, valorCarro;
 
-        Scanner Entrada = new Scanner(System.in);
+        try (Scanner Entrada = new Scanner(System.in)) {
+            do {
+                desconto = 0.0;
+                avista = false;
+                carroNovo = false;
+                seguir = false;
 
-        do {
-            desconto = 0.0;
-            avista = false;
-            carroNovo = false;
-            seguir = false;
+                System.out.println("PROGRAMA PARA VENDA DE CARRO \n\n\nDigite o valor do carro (ex: 75000):");
+                valorCarro = Entrada.nextDouble();
 
-            System.out.println("PROGRAMA PARA VENDA DE CARRO \n\n\nDigite o valor do carro (ex: 75000):");
-            valorCarro = Entrada.nextDouble();
+                System.out.println("O pagamento será à vista? Sim (Aperte 1), Não (apertou outro número)");
+                aux = Entrada.nextInt();
 
-            System.out.println("O pagamento será à vista? Sim (Aperte 1), Não (apertou outro número)");
-            aux = Entrada.nextInt();
+                if (aux == 1) 
+                    avista = true;
 
-            if (aux == 1) 
-                carroNovo = true;
-            
-            if (avista && carroNovo) {
-                desconto = 0.15;
-            } else if (avista) {
-                desconto = 0.08;
-            } else if (carroNovo) {
-                desconto = 0.05;
-            } else {
-                System.out.println("Ofereça um café para o cliente.");
-            }
+                System.out.println("O carro escolhido é 0km? Sim (Aperte 1), Não (apertou outro número)");
+                aux = Entrada.nextInt();
+                
+                if (aux == 1) 
+                    carroNovo = true;
+                
+                if (avista && carroNovo) {
+                    desconto = 0.15;
+                } else if (avista) {
+                    desconto = 0.08;
+                } else if (carroNovo) {
+                    desconto = 0.05;
+                } else {
+                    System.out.println("Ofereça um café para o cliente.");
+                }
 
-            valorCarro -= valorCarro * desconto;
+                valorCarro -= valorCarro * desconto;
 
-            System.out.println("Valor final para pagamento:" + valorCarro);
-            System.out.println("\n\n\n\n\nCalcular o valor de uma nova venda (Aperte 1).");
-            System.out.println("Encerrar o programa             (Aperte outro número)");
+                System.out.println("Valor final para pagamento:" + valorCarro);
+                System.out.println("\n\n\n\n\nCalcular o valor de uma nova venda (Aperte 1).");
+                System.out.println("Encerrar o programa             (Aperte outro número)");
 
-            aux = Entrada.nextInt();
+                aux = Entrada.nextInt();
 
-            if (aux == 1)
-                seguir = true;
-        } while (seguir);
+                if (aux == 1)
+                    seguir = true;
+            } while (seguir);
+        }
     }
 }
